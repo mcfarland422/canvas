7a67578f4bcda4653048e385d0327d8bd599c77b
Michael McFarland <gardengrove422@gmail.com>
2018-03-09 17:56:51 -0500

    first

---
+++ canvas.js
@@ -0,0 +1,31 @@
+var canvas = document.querySelector('canvas')
+  ;
+
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
+
+var c = canvas.getContext('2d');
+
+// Rectangle
+c.fillStyle = 'rgba(255, 0, 0, 0.5)';
+// start point x and y
+// c.fillRect( x, y, width, height)
+c.fillRect(100, 100, 100, 100);
+c.fillStyle = 'rgba(0, 0, 255, 0.5)';
+c.fillRect(400, 100, 100, 100);
+c.fillStyle = 'rgba(0, 255, 0, 0.5)';
+c.fillRect(300, 300, 100, 100);
+
+// Line
+c.beginPath();
+c.moveTo(50, 300);
+c.lineTo(300, 100);
+c.lineTo(400, 300);
+c.strokeStyle = "#fa34a3";
+c.stroke();
+
+// Arc / Circt
+c.beginPath();
+c.arc(300, 300, 30, 0, Math.PI * 2, false);
+c.strokeStyle = 'blue';
+c.stroke();
