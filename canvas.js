987daffd5478ca37ca03c6e2bc8b711181b56e7e
Michael McFarland <gardengrove422@gmail.com>
2018-03-09 20:02:21 -0500

    circles stuck

--- canvas.js
+++ canvas.js
@@ -42,6 +42,9 @@ var c = canvas.getContext('2d');
 function Circle(x, y) {
   this.x = x;
   this.y = y;
+  this.dx = dx;
+  this.dy = dy;
+  this.radius = radius;
 
   this.draw = function() {
     c.beginPath();
@@ -49,37 +52,63 @@ function Circle(x, y) {
     c.strokeStyle = 'red';
     c.stroke();
   }
+
+  this.update = function() {
+    if (this.x + this.radius > innerWidth || this.x - this.radius < 0) {
+      this.dx = -this.dx;
+    }
+
+    if (this.y + this.radius > innerHeight || this.y - this.radius < 0) {
+      this.dy = -this.dy;
+    }
+
+    this.x += this.dx;
+    this.y += this.dy;
+
+    this.draw();
+  }
 }
 
-var circle = new Circle(200, 200)
+// var circle = new Circle(200, 200);
+var circleArray = [];
 
-var x = Math.random() * innerWidth;
-var y = Math.random() * innerHeight;
-// dx is volocity
-var dx = (Math.random() - 0.5) *8;
-var dy = (Math.random() - 0.5) *8;
-var radius = 30;
+for (var i = 0; i < 100; i++) {
+  var radius = 30;
+  var x = Math.random() * innerWidth;
+  var y = Math.random() * innerHeight;
+  // dx is volocity
+  var dx = (Math.random() - 0.5) *8;
+  var dy = (Math.random() - 0.5) *8;
+  circleArray.push(new Circle(x, y, dx, dy, radius));
+
+}
+
+console.log(circleArray);
 
 function animate() {
   requestAnimationFrame(animate);
   c.clearRect(0, 0, innerWidth, innerHeight);
 
+  for (var i=0; i < circleArray.length; i++) {
+    circleArray[i].update();
+  }
+
   circle.draw();
 
-  c.beginPath();
-  c.arc(x, y, radius, 0, Math.PI * 2, false);
-  c.strokeStyle = 'blue';
-  c.stroke();
-
-  if (x + radius > innerWidth || x - radius < 0) {
-    dx = -dx;
-  }
-  if (y + radius > innerHeight || y - radius < 0) {
-    dy = -dy;
-  }
-
-  x += dx;
-  y += dy;
+  // c.beginPath();
+  // c.arc(x, y, radius, 0, Math.PI * 2, false);
+  // c.strokeStyle = 'blue';
+  // c.stroke();
+  //
+  // if (x + radius > innerWidth || x - radius < 0) {
+  //   dx = -dx;
+  // }
+  // if (y + radius > innerHeight || y - radius < 0) {
+  //   dy = -dy;
+  // }
+  //
+  // x += dx;
+  // y += dy;
 }
 
 animate();
